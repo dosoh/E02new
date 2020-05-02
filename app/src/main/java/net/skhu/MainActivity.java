7ae2eb152809bea8404b30353b03e2c210bb9154
Kim, Hyojin <dosoh77@gmail.com>
2020-05-02 21:32:10 +0900

    데이터입력 회원가입(아이디 패스워드 이메일) 화면전환

--- app/src/main/java/net/skhu/MainActivity.java
+++ app/src/main/java/net/skhu/MainActivity.java
@@ -2,7 +2,11 @@ package net.skhu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_signUp) {
+            Intent intent = new Intent(this.SignupActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_memo) {
+            Toast.makeText(this, "메모장 메뉴 클릭", Toast.LENGTH_SHORT).show();
+            return true;
+        } return super.onOptionsItemSelected(item);
     }
 }
